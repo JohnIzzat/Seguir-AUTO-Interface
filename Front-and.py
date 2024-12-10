78d587357a96fb4a8b98b9d732c006df4b5b0015
Izzat <jhonatan.morais2001@gmail.com>
2024-12-10 14:20:12 -0300

    update

--- Front-and.py
+++ Front-and.py
@@ -9,17 +9,37 @@ janela = ctk.CTk()
 janela.title("Sistema de Login")
 janela.geometry("400x300")
 
+
+# Sub Janela
+def abrir_nova_tela():
+    nova_tela = ctk.CTkToplevel()
+    nova_tela.title("Menu de Opções")
+    nova_tela.geometry("400x300")
+
+    titulo_menu = ctk.CTkLabel(nova_tela, text="Menu de Opções")
+    titulo_menu.pack(pady=20)
+
+    botao_opcao1 = ctk.CTkButton(nova_tela, text="Opção 1", command=lambda: print("Opção 1 selecionada"))
+    botao_opcao1.pack(pady=10)
+
+    botao_opcao2 = ctk.CTkButton(nova_tela, text="Opção 2", command=lambda: print("Opção 2 selecionada"))
+    botao_opcao2.pack(pady=10)
+
+    botao_opcao3 = ctk.CTkButton(nova_tela, text="Opção 3 ", command=lambda: print("Opção 3 selecionada"))
+    botao_opcao3.pack(pady=10)
+
+    botao_sair = ctk.CTkButton(nova_tela, text="Sair", command=nova_tela.destroy)
+    botao_sair.pack(pady=20)
+
+
 # Função de verificação do login
-
-
 def verificar_login():
     usuario = entrada_usuario.get()
     senha = entrada_senha.get()
 
-    # Credenciais para teste (pode ser substituído por um banco de dados no futuro)
     if usuario == "admin" and senha == "1234":
-        mensagem["text"] = "Login bem-sucedido!"
-        mensagem.configure(text_color="green")
+        janela.withdraw()
+        abrir_nova_tela()
     else:
         mensagem["text"] = "Usuário ou senha incorretos."
         mensagem.configure(text_color="red")
